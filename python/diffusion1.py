db60e9da0779c6771a5940b5ddc826f2bfc5db9f
Margaret Sledjeski <sledjeski_ma@cobra.cs.mercer.edu>
2017-10-09 14:51:10 -0400

    editing with java class file

--- python/diffusion1.py
+++ python/diffusion1.py
@@ -18,6 +18,7 @@ D=0.175
 elementList = []
 tstep=(Lroom/urms)/Msize
 h=Lroom/Msize
+Dterm = (D*tstep)/(h*h)
 fc=10 #sets the first cube to 100
 
 N=10
@@ -43,35 +44,39 @@ print "Cmin/Cmax =", x
 while x <= .99:
     tacc = tacc + tstep
     #keeps position in the room
-    for l in range(0,N):
-        for m in range(0,N):
-            for n in range(0,N):
+    for l in range(l,N):
+        for m in range(m,N):
+            for n in range(n,N):
                 #access each volume cube from here
                 #print A[0][0][0]
-                i=l
-                j=m
-                k=n
+               # i=l
+               # j=m
+               # k=n
                 for i in range(i,N):
                     for j in range(j,N):
                         for k in range(k,N):
-                            if i > 0 and j > 0:
-                                A[i][j][k] = A[i-1][j-1][k]-2
-                            elif j > 0:
-                                A[i][j][k] = A[i][j-1][k]-1
-                            elif i > 0:
-                                A[i][j][k] = A[i-1][j][k]-1
-                            else:
-                                A[i][j][k] = i*N*N-j*N-k-1+fc
-                                A[i][j][k] = A[i][j][k]+1
-                        elementList.append(A[i][j][k])
-                print "A[%d][%d][%d] = %d" % (i, j, k, A[i][j][k])
-                ma = Cma(*elementList)
-                mi = Cmi(*elementList)
-                x = mi/float(ma)
-                print "Cmax =", ma
-                print "Cmin =", mi
-                print "Cmin/Cmax =", x
-                elementList = []
+                            if (l==i and m==j and n==k+1) or (l==i and m==j and n==k-1) or (l==i and m==j+1 and n==k) or (l==i and m==j-1 and n==k) or (l==i+1 and m==j and n==k) or (l==i-1 and m==j and n==k)
+                                temp = (A[l][m][n]-A[i][j][k])*Dterm
+                                A[l][m][n] = A[l][m][n] - temp
+                                A[i][j][k] = A[i][j][k] + temp
+                #            if i > 0 and j > 0:
+                #                A[i][j][k] = A[i-1][j-1][k]-2
+                #            elif j > 0:
+                #                A[i][j][k] = A[i][j-1][k]-1
+                #            elif i > 0:
+                #                A[i][j][k] = A[i-1][j][k]-1
+                #            else:
+                #                A[i][j][k] = i*N*N-j*N-k-1+fc
+                #                A[i][j][k] = A[i][j][k]+1
+                #        elementList.append(A[i][j][k])
+                #print "A[%d][%d][%d] = %d" % (i, j, k, A[i][j][k])
+                #ma = Cma(*elementList)
+                #mi = Cmi(*elementList)
+                #x = mi/float(ma)
+                #print "Cmax =", ma
+                #print "Cmin =", mi
+                #print "Cmin/Cmax =", x
+                #elementList = []
     #ma = Cma(*elementList)
     #mi = Cmi(*elementList)
     #x = mi/float(ma)
