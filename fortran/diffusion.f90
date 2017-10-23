3eef3dea9350ab8f67bef316c25f0dc92a10be71
Margaret Sledjeski <sledjeski_ma@cobra.cs.mercer.edu>
2017-10-23 12:36:20 -0400

    confirmed sumval works, still not going through the cube right

--- fortran/diffusion.f90
+++ fortran/diffusion.f90
@@ -44,24 +44,40 @@ end do
 
 !cube(0,0,0)=1000000000000000000000_real64
 cube(0,0,0)=1.0e21!1.0*10.0**21!1.0e21
+cube(mdim,mdim,mdim)=10
+!print *, cube(mdim,mdim,mdim)
 do while (ratio<0.99)
     do i=1,mdim
+!        if ((i.eq.l .and. j.eq.m .and. k.eq.n-1).or.(i.eq.l .and. j.eq.m .and. k.eq.n+1)&
+!            .or.(i.eq.l .and. j.eq.m+1 .and. k.eq.n).or.(i.eq.l .and. j.eq.m-1 .and. k.eq.n)&
+!            .or.(i.eq.l+1 .and. j.eq.m .and. k.eq.n).or.(i.eq.l-1 .and. j.eq.m .and. k.eq.n)) then
+!            change=(cube(i,j,k)-cube(l,m,n))*DTerm
+!            cube(i,j,k)=cube(i,j,k)-change
+!            cube(l,m,n)=cube(l,m,n)+change
+!        end if
         do j=1,mdim
             do k=1,mdim
                 do l=1,mdim
                     do m=1,mdim
                         do n=1,mdim
-                            if ((i.eq.l .and. j.eq.m .and. k.eq.n-1).or.(i.eq.l .and. j.eq.m .and. k.eq.n+1)&
-                                .or.(i.eq.l .and. j.eq.m+1 .and. k.eq.n).or.(i.eq.l .and. j.eq.m-1 .and. k.eq.n)&
-                                .or.(i.eq.l+1 .and. j.eq.m .and. k.eq.n).or.(i.eq.l-1 .and. j.eq.m .and. k.eq.n)) then
-                                change=(cube(i,j,k)-cube(l,m,n))*DTerm
-                                cube(i,j,k)=cube(i,j,k)-change
-                                cube(l,m,n)=cube(l,m,n)+change
-                            end if
+!                            if ((i.eq.l .and. j.eq.m .and. k.eq.n-1).or.(i.eq.l .and. j.eq.m .and. k.eq.n+1)&
+!                                .or.(i.eq.l .and. j.eq.m+1 .and. k.eq.n).or.(i.eq.l .and. j.eq.m-1 .and. k.eq.n)&
+!                                .or.(i.eq.l+1 .and. j.eq.m .and. k.eq.n).or.(i.eq.l-1 .and. j.eq.m .and. k.eq.n)) then
+!                                change=(cube(i,j,k)-cube(l,m,n))*DTerm
+!                                cube(i,j,k)=cube(i,j,k)-change
+!                                cube(l,m,n)=cube(l,m,n)+change
+!                            end if
                         end do
                     end do
                 end do
             end do
+            if ((i.eq.l .and. j.eq.m .and. k.eq.n-1).or.(i.eq.l .and. j.eq.m .and. k.eq.n+1)&
+                .or.(i.eq.l .and. j.eq.m+1 .and. k.eq.n).or.(i.eq.l .and. j.eq.m-1 .and. k.eq.n)&
+                .or.(i.eq.l+1 .and. j.eq.m .and. k.eq.n).or.(i.eq.l-1 .and. j.eq.m .and. k.eq.n)) then
+                change=(cube(i,j,k)-cube(l,m,n))*DTerm
+                cube(i,j,k)=cube(i,j,k)-change
+                cube(l,m,n)=cube(l,m,n)+change
+            end if
         end do
     end do
     
@@ -70,17 +86,29 @@ do while (ratio<0.99)
     maxelement=cube(0,0,0)!maxval(cube)
     minelement=cube(0,0,0)!minval(cube)
     do i=1,mdim
+!        if (maxelement<cube(i,j,k)) then!sumval=sumval+cube(i,j,k)
+!            maxelement=cube(i,j,k)
+!        end if
+!        if (minelement>cube(i,j,k)) then
+!            minelement=cube(i,j,k)
+!        end if
         do j=1,mdim
             do k=1,mdim
-                if (maxelement<cube(i,j,k)) then!sumval=sumval+cube(i,j,k)
-                    maxelement=cube(i,j,k)
-                end if
-                if (minelement>cube(i,j,k)) then
-                    minelement=cube(i,j,k)
-                end if
+!                if (maxelement<cube(i,j,k)) then!sumval=sumval+cube(i,j,k)
+!                    maxelement=cube(i,j,k)
+!                end if
+!                if (minelement>cube(i,j,k)) then
+!                    minelement=cube(i,j,k)
+!                end if
                 !sumval=sumval+cube(i,j,k)
             end do
         end do
+        if (maxelement<cube(i,j,k)) then!sumval=sumval+cube(i,j,k)
+            maxelement=cube(i,j,k)
+        end if
+        if (minelement>cube(i,j,k)) then
+            minelement=cube(i,j,k)
+        end if
     end do
     sumval=sum(cube)
     ratio=minelement/maxelement
