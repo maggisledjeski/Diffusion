6e18cfa7fc9baf1982775df12410f2aeb6032585
Margaret Sledjeski <sledjeski_ma@cobra.cs.mercer.edu>
2017-10-24 10:49:15 -0400

    sumval=1.00000002E+21

--- fortran/diffusion.f90
+++ fortran/diffusion.f90
@@ -42,27 +42,17 @@ do i=1,mdim
     end do
 end do
 
-!cube(0,0,0)=1000000000000000000000_real64
-cube(0,0,0)=1.0e21!1.0*10.0**21!1.0e21
-!cube(mdim,mdim,mdim)=10
-!print *, cube(mdim,mdim,mdim)
+cube(1,1,1)=1.0e21
 do while (ratio<0.99)
     do i=1,mdim
-!        if ((i.eq.l .and. j.eq.m .and. k.eq.n-1).or.(i.eq.l .and. j.eq.m .and. k.eq.n+1)&
-!            .or.(i.eq.l .and. j.eq.m+1 .and. k.eq.n).or.(i.eq.l .and. j.eq.m-1 .and. k.eq.n)&
-!            .or.(i.eq.l+1 .and. j.eq.m .and. k.eq.n).or.(i.eq.l-1 .and. j.eq.m .and. k.eq.n)) then
-!            change=(cube(i,j,k)-cube(l,m,n))*DTerm
-!            cube(i,j,k)=cube(i,j,k)-change
-!            cube(l,m,n)=cube(l,m,n)+change
-!        end if
         do j=1,mdim
             do k=1,mdim
                 do l=1,mdim
                     do m=1,mdim
                         do n=1,mdim
-                            if ((i.eq.l .and. j.eq.m .and. k.eq.n-1).or.(i.eq.l .and. j.eq.m .and. k.eq.n+1)&
-                                .or.(i.eq.l .and. j.eq.m+1 .and. k.eq.n).or.(i.eq.l .and. j.eq.m-1 .and. k.eq.n)&
-                                .or.(i.eq.l+1 .and. j.eq.m .and. k.eq.n).or.(i.eq.l-1 .and. j.eq.m .and. k.eq.n)) then
+                            if (((i.eq.l).and.(j.eq.m).and.(k.eq.n-1)).or.((i.eq.l).and.(j.eq.m).and.(k.eq.n+1))&
+                                .or.((i.eq.l).and.(j.eq.m+1).and.(k.eq.n)).or.((i.eq.l).and.(j.eq.m-1).and.(k.eq.n))&
+                                .or.((i.eq.l+1).and.(j.eq.m).and.(k.eq.n)).or.((i.eq.l-1).and.(j.eq.m).and.(k.eq.n))) then
                                 change=(cube(k,j,i)-cube(n,m,l))*DTerm
                                 cube(k,j,i)=cube(k,j,i)-change
                                 cube(n,m,l)=cube(n,m,l)+change
@@ -71,54 +61,32 @@ do while (ratio<0.99)
                     end do
                 end do
             end do
-!            if ((i.eq.l .and. j.eq.m .and. k.eq.n-1).or.(i.eq.l .and. j.eq.m .and. k.eq.n+1)&
-!                .or.(i.eq.l .and. j.eq.m+1 .and. k.eq.n).or.(i.eq.l .and. j.eq.m-1 .and. k.eq.n)&
-!                .or.(i.eq.l+1 .and. j.eq.m .and. k.eq.n).or.(i.eq.l-1 .and. j.eq.m .and. k.eq.n)) then
-!                change=(cube(i,j,k)-cube(l,m,n))*DTerm
-!                cube(i,j,k)=cube(i,j,k)-change
-!                cube(l,m,n)=cube(l,m,n)+change
-!            end if
         end do
     end do
     
     time=time+timestep
     sumval=0.0
-    maxelement=cube(0,0,0)!maxval(cube)
-    minelement=cube(0,0,0)!minval(cube)
+    maxelement=cube(1,1,1)
+    minelement=cube(1,1,1)
     do i=1,mdim
-!        if (maxelement<cube(i,j,k)) then!sumval=sumval+cube(i,j,k)
-!            maxelement=cube(i,j,k)
-!        end if
-!        if (minelement>cube(i,j,k)) then
-!            minelement=cube(i,j,k)
-!        end if
         do j=1,mdim
             do k=1,mdim
-                if (maxelement<cube(k,j,i)) then!sumval=sumval+cube(i,j,k)
+                if (maxelement<cube(k,j,i)) then
                     maxelement=cube(k,j,i)
                 end if
                 if (minelement>cube(k,j,i)) then
                     minelement=cube(k,j,i)
                 end if
-                !sumval=sumval+cube(i,j,k)
+                sumval=sumval+cube(k,j,i)
             end do
         end do
-!        if (maxelement<cube(i,j,k)) then!sumval=sumval+cube(i,j,k)
-!            maxelement=cube(i,j,k)
-!        end if
-!        if (minelement>cube(i,j,k)) then
-!            minelement=cube(i,j,k)
-!        end if
     end do
-    sumval=sum(cube)
     ratio=minelement/maxelement
     print *,time," ",ratio," ",sumval
 
 end do
     
 print *,"Box equilibrated in ",time," seconds of simulated time."
-
-!forall(i=1:mdim,j=1:mdim,k=1:mdim)cube(i,j,k)=1.0
 
 END SUBROUTINE fill_cube
 
