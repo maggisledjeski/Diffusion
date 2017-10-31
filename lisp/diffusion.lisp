5c42c8d9f75287193f8b5b1dc53ff4e09d8809b7
Margaret Sledjeski <sledjeski_ma@cobra.cs.mercer.edu>
2017-10-31 12:07:34 -0400

    added initial value to 0,0,0

--- lisp/diffusion.lisp
+++ lisp/diffusion.lisp
@@ -2,19 +2,24 @@
 
 (defvar A)
 
-(setf A(make-array'(5 5 5)))
+(setf A(make-array'(10 10 10)))
 
-(dotimes (i 5)
-    (dotimes (j 5)
-        (dotimes (k 5)
+;sets the initial value for A to 0
+(dotimes (i 10)
+    (dotimes (j 10)
+        (dotimes (k 10)
             (setf (aref A i j k)(list i '_ j' _ k'= 0))
 ;            (setf (aref A i j k)(list i 'x j' x k'=(* i j k)))
         )
     )
 )
-(dotimes (i 5)
-    (dotimes (j 5)
-        (dotimes (k 5)
+;sets the initial value
+(setf (aref A 0 0 0)'100)
+
+;prints the mutli array
+(dotimes (i 10)
+    (dotimes (j 10)
+        (dotimes (k 10)
             (print (aref A i j k))
 ;            (write-line "string") ;\n affect after write-line
         )
