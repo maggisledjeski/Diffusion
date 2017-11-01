4eda1b11f8835d5f126febe6116f9d4422123f2e
Margaret Sledjeski <sledjeski_ma@cobra.cs.mercer.edu>
2017-11-01 12:40:46 -0400

    added switch code, need to move code around

--- c/diffusion.c
+++ c/diffusion.c
@@ -6,17 +6,43 @@
 // YOU really should look into the fmax and fmin function to simplify and speed up your program
 
 
-double findRatio(double ***A, int N);
-
 int main(int argc, char** argv)
 {
-    const int N = 10;
-    const float x = 10.0;
+    //Varibles that are the same for setting up the cube with or without the partition
+    const int N = 10;                               //maxsize
+    const float x = 10.0;                           //maxsize in a float
     int i,j,k,l,m,n;
-    float change;
-    int fc = 10;
-    float*** A = malloc(N*sizeof(float***));
+    float*** A = malloc(N*sizeof(float***));        //the multidimensional array
+    float D = 0.175;                                //diffusion coeficient
+    float room = 5.0;                               //room dimension is 5 meters
+    float speed = 250.0;                            //speed of gas molecule based on 100 g/mol gas at RT
+    float timestep = (room/speed)/x;                //basis for spatial stepsizes with respect to position in seconds
+    float disblock = room/x;                        //is the distance between blocks
+    float DTerm = D*timestep/(disblock*disblock);   //
+    float time = 0.0;                               //tracks the simulated time
+    float ratio = 0.0;                              //the ratio of the min concentration/the max concentration
+    float change;                                   //is the change of concentrations between blocks
 
+    //get user input do run the program with a partition or not
+    char type;
+    printf("Do you want to run this program with a partition?\n");
+    printf("Enter y for partition, or enter n to run without a partition:\n");
+    scanf("%c", &type);
+    switch (type)
+    {
+        case 'y':
+            printf("This program will run with a partition...\n");
+            break;
+        case 'n':
+            printf("This program will run without a partition...\n");
+            break;
+        default:
+            printf("invalid input\n");
+    }
+
+
+
+    //Allocating the space for the 3d array
     for(i=0;i<N;i++)
     {
         A[i] = malloc(N*sizeof(float*));
@@ -25,7 +51,8 @@ int main(int argc, char** argv)
             A[i][j] = malloc(N*sizeof(float));
         }
     }
-    //set 3d array to 0 except for first cube
+
+    //set 3d array to 0 except for first cube which is set to 1.0e21
     for(i=0;i<N;i++)
     {
         for(j=0;j<N;j++)
@@ -44,15 +71,9 @@ int main(int argc, char** argv)
         }
     }
     
-    float D = 0.175;
-    float room = 5.0;
-    float speed = 250.0;
-    float timestep = (room/speed)/x;
-    float disblock = room/x;
-    float DTerm = D*timestep/(disblock*disblock);
-    float time = 0.0;
-    float ratio = 0.0;
-
+    //goes through each block of two versions (original(i,j,k) and new(l,n,m)) and compares the concentration (ratio) of every block next to it
+    //the change (change of concentration) between the ratios is subtracted from the original and added to the new, in order to show the concentration
+    //of the gas changes as time continues
     while(ratio < 0.99)
     {
         time = time + timestep;
@@ -79,11 +100,13 @@ int main(int argc, char** argv)
                     }
                 } 
             }
-        }//end of for loops
+        }
         
-        float sumval = 0.0;
-        float max = A[0][0][0];
-        float min = A[0][0][0];
+        //check for mass consistency: to make sure that we are accounting for every molecule of gas, and that none of the gas goes outside the cube
+        //determines the new ratio after 1 step: divides the new min by the new max
+        float sumval = 0.0;         //sum of the concentrations of every block in the cube
+        float max = A[0][0][0];     //block with the highest concentration
+        float min = A[0][0][0];     //block with the lowest concentration
     
         for(i=0;i<N;i++)
         {
@@ -91,55 +114,18 @@ int main(int argc, char** argv)
             {
                 for(k=0;k<N;k++)
                 {
-                    max = fmax(max,A[i][j][k]);
-                    min = fmin(min,A[i][j][k]);
-//                    if(max < A[i][j][k])
-//                    {
-//                        max = A[i][j][k];
-//                    }
-//                    if(min > A[i][j][k])
-//                    {
-//                        min = A[i][j][k];
-//                    }
-                    sumval += A[i][j][k];
+                    max = fmax(max,A[i][j][k]);     //fmax returns the bigger concentratio and sets it as the max value
+                    min = fmin(min,A[i][j][k]);     //fmin returns the smaller concentratio and sets it as the min value
+                    sumval += A[i][j][k];           
                 }
             }
         }
         ratio = min/max;
 
         printf("time=%f",time);
-        printf(" ratio=%f\n",ratio);
+        printf(" ratio=%f",ratio);
         printf(" sumval=%f\n",sumval);
     }
     printf("The box equilibrated in %f, seconds of simulated time.",time);
     free(A);
-}
-
-double findRatio(double ***A, int N)
-{
-    int i,j,k;
-    double max = A[0][0][0];
-    double min = A[0][0][0];
-    double ratio = min/max;
-    printf("%f\n",max);
-    for(i=0;i<N;i++)
-    {
-        for(j=0;j<N;j++)
-        {
-            for(k=0;k<N;k++)
-            {
-                if(max < A[i][j][k]) 
-                {
-                    max = A[i][j][k];
-                }
-                if(min > A[i][j][k])
-                {
-                    min = A[i][j][k];
-                }
-            }
-        }
-    }
-    ratio = min/max;
-    //printf("%f\n",ratio);
-    return ratio;
 }
