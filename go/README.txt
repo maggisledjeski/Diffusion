569d7d88f72d7ba3c95655fffa6ae0ba780b1dcc
Margaret Sledjeski <sledjeski_ma@cobra.cs.mercer.edu>
2017-11-09 16:36:01 -0500

    added README and comments

---
+++ go/README.txt
@@ -0,0 +1,28 @@
+README FILE FOR GO CODE:
+
+In this repository the file, diffusion.go, contains the code of my project.
+This file asks for user input on how to run the program, using a switch statement with cases to navigate whether the user wants to run a partition or not,
+leaving the default case as "Entered an invalid input" if the user does not answer the prompt correctly.
+
+In order to run this program correctly, please read the following:
+
+TO RUN WITHOUT A PARTITION:
+Compile and run the code by typing: go run diffusion.go
+The code will prompt you with: "To run program with a partition please enter 'yes', to run without a partition please enter 'no':"
+Type: no
+Then press the enter key.
+You will see a confirmation statement: "This program will run without a partition..."
+If you do not enter either yes or no, "Entered an invalid input" is printed to the screen and the program will end.
+Then the simulated time will appear on the screen after each step is taken. In order to see the ratio and the mass consistency printed uncomment the lines.
+After the ratio has reached 0.99 the program will print: "The box equilibrated in (insert time here), seconds of simulated time without a partition." 
+
+TO RUN WITH A PARTITION:
+Compile and run the code by typing: go run diffusion.go
+The code will prompt you with: "To run program with a partition please enter 'yes', to run without a partition please enter 'no':"
+Type: yes
+Then press the enter key.
+You will see a confirmation statement: "This program will run with a partition..."
+If you do not enter either yes or no, "Entered an invalid input" is printed to the screen and the program will end.
+Then the simulated time will appear on the screen after each step is taken. In order to see the ratio and the mass consistency printed uncomment the lines.
+After the ratio has reached 0.99 the program will print: "The box equilibrated in (insert time here), seconds of simulated time with a partition."
+
--- go/diffusion.go
+++ go/diffusion.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+     
     //Varibles that are the same for setting up the cube with or without the partition
     const N int = 10                        //maxsize
     var A[N][N][N]float64                   //cube declaration of size NxNxN
@@ -18,20 +19,21 @@ func main() {
     timestep := (room/speed)/x              //basis for spatial stepsizes with respect to position in seconds
     disblock := room/x                      //is the distance between blocks
     DTerm := D*timestep/(disblock*disblock) //
-    time := 0.0
-    ratio := 0.0
+    time := 0.0                             //tracks simulated time
+    ratio := 0.0                            //the ratio of the min concentration/the max concentration
 
     //User input: enter yes in order to run the program with a partition. The default case will run without a partition.
-    fmt.Print("To run program with a partition please enter 'yes': ")
+    fmt.Print("To run program with a partition please enter 'yes', to run without a partition please enter 'no': ")
     var input string
     var lowerinput string
     fmt.Scanln(&input)
     lowerinput = strings.ToLower(input) //changes the input to lower case
     fmt.Println(strings.ToLower(input))
+    
     switch lowerinput {
         case "yes":
-            //the program will run with a partition
-            fmt.Println("will run with a partition...")
+            //***start of partition code***
+            fmt.Println("This program will run with a partition...")
 
             //initializes the initial values of the cube as well as the walls
             for i=0; i < N; i++ {
@@ -48,49 +50,41 @@ func main() {
                     }
                 }
             }
-            A[0][0][0] = 1.0e21
-//            to print
-            for i=0; i < N; i++ {
-                for j=0; j<N; j++ {
-                    for k=0; k<N; k++ {
-                        fmt.Print(A[i][j][k] );
-                    }
-                    fmt.Println();
-                }
-                fmt.Println();
-            }
+            A[0][0][0] = 1.0e21     //initializes the first cube to 1.0e21
             
+            //Goes through each block of two versions of the room (original[i][j][k]) and new([l][n][m])) and compares the concentration (ratio) 
+            //of every block next to it as long as the current block is not a wall. Also, the if statement containing all of the cases has an extra 
+            //case to check if that specific block next to the current is not a wall ex. A[l][m][n+1]!=-1. The change (change of concentration) 
+            //between the ratios is subtracted from the original and added to the new, in order to show the concentration of the gas changes 
+            //as simulated time continues.
+
             for ratio<0.99 {
-                //fmt.Printf("%d %d %d\n", i, j, k)
-                time = time + timestep
+                time = time + timestep  //increments the simulated time
                 for i=0; i<N; i++ {
                     for j=0; j<N; j++ {
                         for k=0; k<N; k++ {
                             for l=0; l<N; l++ {
                                 for m=0; m<N; m++ {
                                     for n=0; n<N; n++ {
-                                        if A[l][m][n] != -1 {
+                                        if A[l][m][n] != -1 {   //checks that the current block is not a wall
                                             if((i==l)&&(j==m)&&(k==n+1)&&(A[l][m][n+1]!=-1))||((i==l)&&(j==m)&&(k==n-1)&&(A[l][m][n-1]!=-1))||((i==l)&&(j==m+1)&&(k==n)&&(A[l][m+1][n]!=-1))||((i==l)&&(j==m-1)&&(k==n)&&(A[l][m-1][n]!=-1))||((i==l+1)&&(j==m)&&(k==n)&&(A[l+1][m][n]!=-1))||((i==l-1)&&(j==m)&&(k==n)&&(A[l-1][m][n]!=-1)) {
                                                 change := (A[i][j][k]-A[l][m][n])*DTerm;
                                                 A[i][j][k] = A[i][j][k] - change;
                                                 A[l][m][n] = A[l][m][n] + change;
-                                                //fmt.Printf("%g ", A[i][j][k]);
-                                               // fmt.Printf("%d %d %d %d %d %d\n", i, j, k, l, m, n)
                                             }
                                         }
                                     }
                                 }
                             }
                         }
-                    //fmt.Printf("%g\n", A[i][j][k])
                     }
-                //fmt.Printf("%g\n", A[i][j][k])
                 } 
-                //fmt.Printf("%d %d %d\n", i, j, k)
-                //fmt.Printf("%g\n", A[i][j][k])
-                sumval := 0.0
-                max := A[0][0][0]
-                min := A[0][0][0]
+
+                //Check for mass consistency: to make sure that we are accounting for every molecule of gas, and that none of the gas goes outside the cube.
+                //Determines the new ratio after every step: divides the new min by the new max.
+                sumval := 0.0         //sum of the concentrations of every block in the cube
+                max := A[0][0][0]     //block with the highest concentration
+                min := A[0][0][0]     //block with the lowest concentration
 
                 for i=0; i<N; i++ {
                     for j=0; j<N; j++ {
@@ -102,21 +96,23 @@ func main() {
                                 if min > A[i][j][k] {
                                     min = A[i][j][k]    //new min if a smaller concentration is found within the cube
                                 }
-                                sumval += A[i][j][k]
+                                sumval += A[i][j][k]    //adds the sum of every block in the cube
                             }
                         }
                     }
                 }
-                ratio = min/max
+                ratio = min/max     //calculates the updated ratio for every step
+
                 fmt.Printf("%g %g %g\n", time, ratio, sumval)
             }
             fmt.Printf("Box equilibrated in %g seconds of simulated time with a partition.\n", time)
-        //end of partition code
-        default:
-            //the program will run without a partition
-            fmt.Println("will run without a partition...")
+        //***end of partition code***
+
+        //***start of non-partition code***
+        case "no":
+            fmt.Println("This program will run without a partition...")
     
-            //initializes the initial values of the cube
+            //initializes the first cube to 1.0e21 and the others to 0.0
             for i=0; i < N; i++ {
                 for j=0; j<N; j++ {
                     for k=0; k<N; k++ {
@@ -129,12 +125,12 @@ func main() {
                 }
             }
     
-            //goes through each block of two versions (original(i,j,k) and new(l,n,m)) and compares the concentration (ratio) of every block next to it
-            //the change (change of concentration) between the ratios is subtracted from the original and added to the new, in order to show the concentration
-            //of the gas changes as time continues
+            //Goes through each block of two versions of the room (original[i][j][k]) and new([l][n][m]))and compares the concentration (ratio)
+            //of every block next to it. The change (change of concentration) between the ratios is subtracted from the original and added to the new,
+            //in order to show the concentration of the gas changes as simulated time continues.
 
             for ratio<0.99 {
-                time = time + timestep
+                time = time + timestep  //increments the simulated time
                 for i=0; i<N; i++ {
                     for j=0; j<N; j++ {
                         for k=0; k<N; k++ {
@@ -153,12 +149,11 @@ func main() {
                     }
                 }
 
-                //check for mass consistency: to make sure that we are accounting for every molecule of gas, and that none of the gas goes outside the cube
-                //determines the new ratio after 1 step: divides the new min by the new max
-
-                sumval := 0.0
-                max := A[0][0][0]
-                min := A[0][0][0]
+                //Check for mass consistency: to make sure that we are accounting for every molecule of gas, and that none of the gas goes outside the cube.
+                //Determines the new ratio after every step: divides the new min by the new max.
+                sumval := 0.0         //sum of the concentrations of every block in the cube
+                max := A[0][0][0]     //block with the highest concentration
+                min := A[0][0][0]     //block with the lowest concentration
 
                 for i=0; i<N; i++ {
                     for j=0; j<N; j++ {
@@ -169,13 +164,19 @@ func main() {
                             if min > A[i][j][k] {
                                 min = A[i][j][k]    //new min if a smaller concentration is found within the cube
                             }
-                            sumval += A[i][j][k]
+                            sumval += A[i][j][k]    //adds the sum of every block in the cube
                         }
                     }
                 }
-                ratio = min/max
+                ratio = min/max     //calculates the updated ratio for every step
+
                 fmt.Printf("%g %g %g\n", time, ratio, sumval)
             } 
             fmt.Printf("Box equilibrated in %g seconds of simulated time without a partition.\n", time)
-    }
+            //***end of non-partition code***
+
+        //if invalid input is entered
+        default:
+            fmt.Printf("Entered an invalid input")
+    }    
 }
